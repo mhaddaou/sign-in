2cf09aa9d8f480715720249e022d679689cb0adc
mhaddaou <mhaddaou@student.1337.ma>
2023-08-18 16:13:14 -0400

    add upload image

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Upload from './Upload';
 
 interface ModalProps{
     isOpen : boolean;
@@ -8,38 +9,38 @@ interface ModalProps{
 const FirstStep = () =>{
     return (
         <div  className='flex text-slate-800 pt-5 '>
-                       <div className='w-[90%] mx-auto  h-full space-y-5'>
+                       <div className='w-[90%] mx-auto  h-full space-y-8'>
                         <div className='w-full flex items-center'>
-                            <div className='w-[30%] pt-6 font-bold'>Name</div>
+                            <div className='w-[30%] pt-6 font-bold text-center'>Name</div>
                             <div className='w-[70%] flex gap-5'>
                                 <div>
                                     <p className='pl-2 text font-light text- capitalize pb-1'>first name<span className='text-slate-600'>*</span></p>
-                                    <input type="text" placeholder="First Name" className="input bg-slate-100 input-bordered font-mono input-md w-full max-w-xs" />
+                                    <input type="text" placeholder="First Name" className="input bg-slate-100 input-primary input-bordered font-mono input-md w-full max-w-xs" />
                                 </div>
                                 <div>
                                     <p className='pl-2 text font-light text- capitalize pb-1'>last name<span className='text-slate-600'>*</span></p>
-                                    <input type="text" placeholder="Last Name" className="input bg-slate-100 input-bordered input-md font-mono w-full max-w-xs" />
+                                    <input type="text" placeholder="Last Name" className="input bg-slate-100 input-bordered  input-primary input-md font-mono w-full max-w-xs" />
                                 </div>
 
 
                             </div>
                         </div>
                         <div className='w-full flex items-center'>
-                            <div className='w-[30%]  font-bold'>Email</div>
+                            <div className='w-[30%]  font-bold text-center'>Email</div>
                             <div className='w-[70%] flex gap-5'>
-                                    <input type="text" placeholder="example@gmail.com" className="input bg-slate-100 input-bordered font-mono input-md w-full " />
+                                    <input type="text" placeholder="example@gmail.com" className="input bg-slate-100 input-bordered input-primary font-mono input-md w-full " />
                             </div>
                         </div>
                         <div className='w-full flex items-center'>
-                            <div className='w-[30%] pb-6 font-bold'>Phone</div>
+                            <div className='w-[30%] pb-6 font-bold text-center'>Phone</div>
                             <div className='w-[70%] flex gap-5'>
                                 <div className='w-[30%]'>
-                                    <input type="text" placeholder="" className="input bg-slate-100 input-bordered input-md font-mono w-full max-w-xs" />
+                                    <input type="text" placeholder="" className="input bg-slate-100 input-bordered input-md input-primary font-mono w-full max-w-xs" />
                                 <p className='pl-2 text  text-sm font-semibold capitalize pt-1'>Area Code</p>
 
                                 </div>
                                 <div className='w-[70%]'>
-                                    <input type="text" placeholder=" " className="input bg-slate-100 input-bordered input-md font-mono w-full max-w-xs" />
+                                    <input type="text" placeholder=" " className="input bg-slate-100 input-bordered input-md input-primary font-mono w-full max-w-xs" />
                                 <p className='pl-2 text  text-sm font-semibold capitalize pt-1'>phone number</p>
 
 
@@ -56,30 +57,30 @@ const FirstStep = () =>{
 const SecondStep = () =>{
     return (
             <div  className='flex text-slate-800 pt-5 '>
-                <div className='w-[90%] mx-auto  h-full space-y-5'>
+                <div className='w-[90%] mx-auto  h-full space-y-8'>
                 <div className='w-full flex items-center'>
-                            <div className='w-[30%]  font-bold'>Headline<span className='text-slate-600'>*</span></div>
+                            <div className='w-[30%]  font-bold text-center'>Headline<span className='text-slate-600'>*</span></div>
                             <div className='w-[70%] flex gap-5'>
                             <textarea className="textarea textarea-primary w-full bg-white" placeholder="Headline"></textarea>
                             </div>
                         </div>
                         <div className='w-full flex items-center'>
-                            <div className='w-[30%]  font-bold'>Education<span className='text-slate-600'>*</span></div>
+                            <div className='w-[30%]  font-bold text-center'>Education<span className='text-slate-600'>*</span></div>
                             <div className='w-[70%] flex gap-5'>
-                                    <input type="text" placeholder="" className="input bg-white input-bordered font-mono input-md w-full " />
+                                    <input type="text" placeholder="" className="input bg-white input-bordered input-primary font-mono input-md w-full " />
                             </div>
                         </div>
                         <div className='w-full flex items-center'>
-                            <div className='w-[30%] pb-6 font-bold'>Location</div>
+                            <div className='w-[30%] pb-6 font-bold text-center'>Location</div>
                             <div className='w-[70%] flex gap-5'>
                                 <div className='w-[40%]'>
-                                    <input type="text" placeholder="" className="input bg-white input-bordered input-md font-mono w-full max-w-xs" />
+                                    <input type="text" placeholder="" className="input bg-white input-bordered input-primary input-md font-mono w-full max-w-xs" />
                                 <p className='pl-2 text  text-sm font-semibold capitalize pt-1'>Country/Region<span className='text-slate-600'>*</span></p>
 
                                 </div>
                                 <div className='w-[60%]'>
-                                    <input type="text" placeholder=" " className="input bg-white input-bordered input-md font-mono w-full max-w-xs" />
-                                <p className='pl-2 text  text-sm font-semibold capitalize pt-1'>City<span className='text-slate-600'>*</span></p>
+                                    <input type="text" placeholder=" " className="input input-primary bg-white input-bordered input-primaryinput-md font-mono w-full max-w-xs" />
+                                <p className='pl-2 text  text-sm font-semibold capitalize  pt-1'>City<span className='text-slate-600'>*</span></p>
 
 
                                 </div>
@@ -87,6 +88,23 @@ const SecondStep = () =>{
                         </div>
                 </div>
             </div>
+    )
+}
+
+
+const ThirdStep = () =>{
+    return (
+        <div  className='flex text-slate-800 pt-5 '>
+            <div className='w-[90%] mx-auto  h-full space-y-8'>
+                <div className='text-center py-10'>
+                    Upload Image
+                </div>
+                <div className='w-1/2  mx-auto'>
+                <Upload />
+
+                </div>
+            </div>
+        </div>
     )
 }
 
@@ -101,9 +119,19 @@ const Modal : React.FC<ModalProps> = ({isOpen, closeModal})=>{
                 <div className='bg-white opacity-100 rounded-2xl p-5'>
                     {num == 1 ? <FirstStep/> : null}
                     {num == 2 ? <SecondStep/> : null}
+                    {num == 3 ? <ThirdStep/> : null}
                     <div className='text-slate-700 py-10 flex justify-end pr-5 gap-10'>
+                    <div>
+                            <button onClick={() => {
+                                if (num !==  1)
+                                    setNum(num - 1)
+                            }}>Previous</button>
+                        </div>
                         <div>
-                            <button onClick={() => setNum(num + 1)}>Next</button>
+                            <button onClick={() => {
+                                if (num !== 3)
+                                    setNum(num + 1)
+                            }}>Next</button>
                         </div>
                         <div>
                             <button onClick={() => closeModal()}>close</button>
---
+++ components/Upload.tsx
@@ -0,0 +1,108 @@
+import axios from "axios";
+import Modal from "@/components/Modal";
+import React, {ChangeEvent, useContext, useEffect, useState} from "react";
+import Image, { StaticImageData } from "next/image";
+
+
+
+const Upload = () =>{
+
+
+    // const context = useContext(MyContext);
+    let imgSrc: string | Blob | MediaSource | StaticImageData;
+    const [url, setUrl] = useState<string | null>(null);
+    const [file, setFile] = useState<File | null>(null)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [title, setTitle] = useState(''); // for title modal
+    const [msg, setMsg] = useState(''); // for msg modal
+    const [color, setColor] = useState(''); // for color modal
+
+
+
+  
+    const uploadImage = () =>{
+
+
+            const form = new FormData();
+    
+            
+            if (file){
+                
+                
+                
+                form.append("file", file);
+                form.append("upload_preset", "mhaddaou");
+                // this file is not jpeg or PNG
+                if ((file.type !== "image/jpeg") && (file.type !== "image/png")){
+                    setColor('bg-orange-600');
+                    setMsg('this image is not jpeg or PNG');
+                    setTitle('Warning!');
+                    setIsModalOpen(true);
+                }
+                else if ((file.size / (1024 * 1024)) > 10){
+                    setColor('bg-orange-600');
+                    setMsg('this image is more than 6MB');
+                    setTitle('Warning!');
+                    setIsModalOpen(true);
+                }
+                else{
+                    try{
+                        axios.post('https://api.cloudinary.com/v1_1/daczu80rh/upload', form)
+                        .then((result) =>{
+                            setUrl(result.data.secure_url);
+                            // context?.setImg(result.data.secure_url);
+                            // sendToBck(result.data.secure_url);
+                            setColor('bg-green-500');
+                            setMsg('The image was successfully uploaded');
+                            setTitle('Success!');
+                            setIsModalOpen(true);
+                        })
+                    }catch(e){}  
+                }
+                setFile(null);
+                
+            }
+            //if the client click in upload without selec(image);
+            else{
+                setColor('bg-orange-600');
+                setMsg('the image is empty');
+                setTitle('Warning!');
+                setIsModalOpen(true);
+                
+                
+            }
+            // }
+        }
+        const handleChange = (e : ChangeEvent<HTMLInputElement>) =>{
+            if (e.target.files )
+            setFile(e.target.files[0]);
+            
+            
+        }
+
+    const openModal = () => {
+      setIsModalOpen(true);
+    };
+
+
+    const closeModal = () => {
+      setIsModalOpen(false);
+    };
+       
+    return (
+        <div className="flex items-center border-b border-slate-600 py-2 justify-center gap-10 pb-5">
+            {/* {isModalOpen && <Modal isOpen={isModalOpen} closeModal={closeModal} title={title} msg={msg} color={color}/>} */}
+                    <label className=" cursor-pointer flex-shrink-0 bg-slate-500 hover:bg-slate-700 border-slate-500 hover:border-slate-700 text-sm border-4 text-white py-1 px-2 rounded" >
+                        <input onChange={handleChange} type="file" className="hidden" />
+                        select file
+                    </label>
+                    <button onClick={() =>{
+                      uploadImage();
+                    }} className=" ml-2 flex-shrink-0 bg-slate-500 hover:bg-slate-700 border-slate-500 hover:border-slate-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                            Upload
+                    </button>
+            </div>
+
+    )
+}
+export default Upload;
