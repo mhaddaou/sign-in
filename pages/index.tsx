733f35821cfcf2313ee944418527a0656578f87d
Mohamed Haddaoui <94408939+mhaddaou@users.noreply.github.com>
2023-08-18 12:16:48 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react'
-import { Dispatch } from './Type';
-import { getUsers } from '@/components/fetSlice'
-function index() {
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'; // Import useDispatch
+import { getUsers } from '@/components/fetSlice';
 
+// Rename the component to start with an uppercase letter
+function Index() {
+  const dispatch = useDispatch(); // Get the dispatch function from react-redux
   
-  useEffect(() =>{
-    Dispatch(getUsers());
-  }, [Dispatch])
+  useEffect(() => {
+    dispatch(getUsers()); // Dispatch the action using the dispatch function
+  }, [dispatch]);
+  
   return (
-   <main className='text-3xl font-extrabold underline font-mono hover:opacity-50 cursor-pointer hover:no-underline  '>
-
-  
-   
-   </main>
-  )
+    <main className='text-3xl font-extrabold underline font-mono hover:opacity-50 cursor-pointer hover:no-underline'>
+      {/* Your component content */}
+    </main>
+  );
 }
 
-
-
-export default index
+export default Index; // Export the component with the updated name
