dd2265a68d65e64ebe8cabc3dc3eddb01afd2bd4
Mohamed Haddaoui <94408939+mhaddaou@users.noreply.github.com>
2023-08-18 12:11:29 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react'
-import Image from 'next/image'
-import { RooteState, AppDispatch } from '@/components/store'
-import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from './Type';
 import { getUsers } from '@/components/fetSlice'
 function index() {
 
-  const dispatch = useDispatch<AppDispatch>();
-
+  
   useEffect(() =>{
-    dispatch(getUsers());
-  }, [dispatch])
+    Dispatch(getUsers());
+  }, [Dispatch])
   return (
    <main className='text-3xl font-extrabold underline font-mono hover:opacity-50 cursor-pointer hover:no-underline  '>
-      
-  nour imane sabri
+
   
    
    </main>
