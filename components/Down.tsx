9017c25cea1fb1da5115cdd8f9542d0f9b19c4ec
mhaddaou <mhaddaou@student.1337.ma>
2023-08-20 16:24:36 -0400

    up

--- components/Down.tsx
+++ components/Down.tsx
@@ -5,7 +5,7 @@ import {AiFillHeart} from 'react-icons/ai'
 function Down() {
   return (
     <section className='bg-eco'>
-        <div className=" md:w-1/2  m-auto text-center   py-2 ">
+        <div className=" md:w-1/2 text-white m-auto text-center   py-2 ">
                     <p className=" f``">Made with <AiFillHeart className="inline w-8 h-8 text-red-600"/> <PiCopyright className=" inline text-"/>  2023 All rights reserved</p>
                 </div>
     </section>
